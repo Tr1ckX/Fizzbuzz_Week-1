72d57f7585e5a4bbcbaf960687b0dafc646a62bd
Tr1ckX <attila.mirk@gmail.com>
2014-10-27 21:23:55 +0000

    tested with 3, 5 and 3&&5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,3 +3,15 @@ def divisible_by_three(number)
   return 'Fizz' if number%3 == 0  
 
 end
+
+def divisible_by_five(number)
+
+  return 'Buzz' if number%5 == 0
+
+end
+
+def divisible_by_three_and_five(number)
+
+  return 'FizzBuzz' if number%3 == 0 && number%5 == 0
+
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,35 @@
 require 'fizzbuzz'
 
-describe 'divisible_by_three' do
+describe 'FizzBuzz' do
 
-    it  'if the number divisible by three returns "Fizz"' do
+    context 'divisible_by_three' do
 
-      expect(divisible_by_three(3)).to eq 'Fizz'
+      it 'if the number is divisible by three returns "Fizz"' do
+
+        expect(divisible_by_three(3)).to eq 'Fizz'
   
+      end
+
+    end
+
+    context 'divisible_by_five' do
+
+      it 'if the number is divisible by five returns "Buzz"' do
+
+        expect(divisible_by_five(5)).to eq 'Buzz'
+
+      end
+
+    end
+
+    context 'divisible_by_three_and_five' do
+
+      it 'if the number is divisible with three and five returns "FizzBuz"' do
+
+        expect(divisible_by_three_and_five(14)).to eq 'FizzBuzz'
+
+      end
+
     end
 
 end
