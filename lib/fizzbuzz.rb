166154be3228661b2465af2e05409bc14b92de3a
Attila <Tr1ckX@attilas-mbp.home>
2014-10-27 20:03:56 +0000

    divisible by 3 tested

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,5 @@
+def divisible_by_three(number)
+  
+  return 'Fizz' if number%3 == 0  
+
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,11 @@
+require 'fizzbuzz'
+
+describe 'divisible_by_three' do
+
+    it  'if the number divisible by three returns "Fizz"' do
+
+      expect(divisible_by_three(3)).to eq 'Fizz'
+  
+    end
+
+end
